c305263851b66407e1f110dd1b1ac4876eeb669d
Andrei <backendmomsorry@gmail.com>
2022-12-01 23:15:11 +0400

    minor fix

--- src/main/kotlin/database/DataSource.kt
+++ src/main/kotlin/database/DataSource.kt
@@ -15,16 +15,12 @@ object DataSource {
             jdbcUrl = "jdbc:postgresql://localhost:5432/postgres"
             username = "postgres"
             password = "postgres"
-            addDataSourceProperty("cachePrepStmts", "true")
-            addDataSourceProperty("prepStmtCacheSize", "250")
-            addDataSourceProperty("prepStmtCacheSqlLimit", "2048")
 
             idleTimeout = 60000;
             maxLifetime = 1800000;
             connectionTestQuery = "SELECT current_timestamp";
             minimumIdle = 20
             validationTimeout = 3000
-//            maximumPoolSize = 500
 
             ds = HikariDataSource(this)
         }
